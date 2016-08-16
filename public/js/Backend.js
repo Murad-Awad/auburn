4520e7f6e435c03a60b1750b3ea081a1b34ef8a4
theiShah <ishu.shah@ymail.com>
2016-08-15 21:50:52 -0700

    Added in basics for typeahead, seems to be an issue getting it to run. Look for tag 'This is the problem on I swear' in Backend.js

--- public/js/Backend.js
+++ public/js/Backend.js
@@ -183,6 +183,156 @@
         })
     };
 
+    var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substringRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+
+    cb(matches);
+  };
+};
+
+var genre_list = [
+    'acoustic',
+    'afrobeat',
+    'alt-rock',
+    'alternative',
+    'ambient',
+    'anime',
+    'black-metal',
+    'bluegrass',
+    'blues',
+    'bossanova',
+    'brazil',
+    'breakbeat',
+    'british',
+    'cantopop',
+    'chicago-house',
+    'children',
+    'chill',
+    'classical',
+    'club',
+    'comedy',
+    'country',
+    'dance',
+    'dancehall',
+    'death-metal',
+    'deep-house',
+    'detroit-techno',
+    'disco',
+    'disney',
+    'drum-and-bass',
+    'dub',
+    'dubstep',
+    'edm',
+    'electro',
+    'electronic',
+    'emo',
+    'folk',
+    'forro',
+    'french',
+    'funk',
+    'garage',
+    'german',
+    'gospel',
+    'goth',
+    'grindcore',
+    'groove',
+    'grunge',
+    'guitar',
+    'happy',
+    'hard-rock',
+    'hardcore',
+    'hardstyle',
+    'heavy-metal',
+    'hip-hop',
+    'holidays',
+    'honky-tonk',
+    'house',
+    'idm',
+    'indian',
+    'indie',
+    'indie-pop',
+    'industrial',
+    'iranian',
+    'j-dance',
+    'j-idol',
+    'j-pop',
+    'j-rock',
+    'jazz',
+    'k-pop',
+    'kids',
+    'latin',
+    'latino',
+    'malay',
+    'mandopop',
+    'metal',
+    'metal-misc',
+    'metalcore',
+    'minimal-techno',
+    'movies',
+    'mpb',
+    'new-age',
+    'new-release',
+    'opera',
+    'pagode',
+    'party',
+    'philippines-opm',
+    'piano',
+    'pop',
+    'pop-film',
+    'post-dubstep',
+    'power-pop',
+    'progressive-house',
+    'psych-rock',
+    'punk',
+    'punk-rock',
+    'r-n-b',
+    'rainy-day',
+    'reggae',
+    'reggaeton',
+    'road-trip',
+    'rock',
+    'rock-n-roll',
+    'rockabilly',
+    'romance',
+    'sad',
+    'salsa',
+    'samba',
+    'sertanejo',
+    'show-tunes',
+    'singer-songwriter',
+    'ska',
+    'sleep',
+    'songwriter',
+    'soul',
+    'soundtracks',
+    'spanish',
+    'study',
+    'summer',
+    'swedish',
+    'synth-pop',
+    'tango',
+    'techno',
+    'trance',
+    'trip-hop',
+    'turkish',
+    'work-out',
+    'world-music'
+  ];
 
     var getNewTracks = function (a, d, e, i, k, li, lo, m, s, te, ti, v, gs) {
         $.ajax({
@@ -266,5 +416,16 @@ z            }
         }
         $("#search").hide();
     }, false);
+    <!-- This is the problem one I swear>
+
+    document.getElementById('genre_search .typeahead').typeahead({
+      hint: true,
+      highlight: true,
+      minLength: 1
+    },
+    {
+      name: 'genre_list',
+      source: substringMatcher(genre_list)
+    }); 
 
 })();
